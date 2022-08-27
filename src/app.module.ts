50d0e891017593634aadb62c2291c8542aa2f971
a2d7cc <a2d7cc@gmail.com>
2022-08-27 13:31:47 +0200

    Backend Cinema App

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { getMongoDbCOnfig } from './config/mongo.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypegooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: getMongoDbCOnfig
+    })
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/config/mongo.config.ts
@@ -0,0 +1,6 @@
+import { ConfigService } from "@nestjs/config";
+import { TypegooseModuleOptions } from "nestjs-typegoose";
+
+export const getMongoDbCOnfig = async (configService: ConfigService): Promise<TypegooseModuleOptions> => ({
+	uri: configService.get('MONGO_URI')
+})
